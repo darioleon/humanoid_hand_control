14f17b11cabd6b3eb124d298a50787f7d0866e88
Darío <darox72leon@hotmail.com>
2017-04-19 13:12:42 -0600

    deleted:    basura.txt

--- bin/run_client_test_ros.sh
+++ bin/run_client_test_ros.sh
@@ -7,10 +7,27 @@ source ../include/HEAD
 # Se ejectuta el script SSH_HAND-SH con el fin de hacer al servidor levantar el proceso escucha
 
 echo "El directorio de HAND_SERVER es: $HAND_SERVER_DIR"
-bash "$HAND_SERVER_DIR""/ssh_hand.sh"
-sleep 1
-xfce4-terminal --command="yarpserver start"
+echo 'Ejecutando $ yarpserver start'
+screen -d -m yarpserver start
 sleep 4
-xfce4-terminal --command="$HAND_SERVER_DIR""/hand_yarp"
-#xfce4-terminal --command='ros-init' --working-directory="$HOME""/catkin_ws" /catkin_ws
-#xfce4-terminal --command="yarpserver start"
+echo "Ejecutando"' $ '"$HAND_SERVER_DIR""/ssh_hand.sh"
+cd "$HAND_SERVER_DIR" # Visita el directorio de HAND_SERVER
+screen -d -m  bash "./ssh_hand.sh"
+sleep 10
+echo "Ejecutando"' $ '"$HAND_SERVER_DIR""/hand_yarp"
+nohup "./hand_yarp" &
+sleep 1
+cd - # Regresa a ./
+echo "Ejecutando ./ros_core_start.sh"
+screen -d -m ./ros_core_start.sh
+echo "Ejecutando ./run_server.sh"
+nohup ./run_server.sh &
+sleep 2
+
+source ~/catkin_ws/devel/setup.bash && xstow -D $HOME/local/DIR/kdl && xstow -D $HOME/local/DIR/kdl-python && source /opt/ros/kinetic/setup.bash
+echo "Ejecutando ../src/client_test_ros.py"
+python ../src/client_test_ros.py
+
+sleep 2
+
+exit 0
